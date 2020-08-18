f2835158ed34989f5691445313f9d2b476f0345d
Itai Mapaya <mambostudioz@gmail.com>
2020-08-18 13:21:52 -0500

    Add behavior

---
+++ js/script.js
@@ -0,0 +1,50 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+//  Get quote from API
+async function getQuote() {
+    const proxyUrl = 'https://pure-tundra-10769.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(`${proxyUrl}${apiUrl}`);
+        const data = await response.json();
+
+        // Get the author's name from API. Use 'Unknown' if author name is unavailable
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+
+        // Reduce font size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+
+        // Get the quote from API
+        quoteText.innerText = data.quoteText;
+    } catch (error) {
+        getQuote();
+    }
+}
+
+// Create a function to tweet a quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+
+// on load 
+getQuote();
